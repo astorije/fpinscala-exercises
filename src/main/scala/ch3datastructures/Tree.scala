3cbbcfdbac94a7609f562798384dd17baf68f1b3
Jérémie Astori <jeremie@astori.fr>
2014-12-27 13:30:09 -0500

    Prepare DataStructures/Tree trait and companion object

---
+++ src/main/scala/ch3datastructures/Tree.scala
@@ -0,0 +1,9 @@
+package ch3datastructures
+
+sealed trait Tree[+A]
+case class Leaf[A](value: A) extends Tree[A]
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+object Tree {
+
+}
