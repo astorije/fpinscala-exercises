0c93628732c1294e1a041513d911486ee4df99cf
Jérémie Astori <jeremie@astori.fr>
2014-12-28 11:24:50 -0500

    Exercise labels for test suites

--- src/test/scala/ch3datastructures/ListSuite.scala
+++ src/test/scala/ch3datastructures/ListSuite.scala
@@ -43,77 +43,95 @@ class ListSuite extends FunSuite {
     assert(length(Nil) === 0)
   }
 
+  // Exercise 3.10
   test("foldLeft") {
     assert(foldLeft(List(1, 2, 3), 0)(_ + _) === 6)
   }
 
+  // Exercise 3.11
   test("sumLeft") {
     assert(sumLeft(List(12, 13, 14)) === 39)
   }
 
+  // Exercise 3.11
   test("productLeft") {
     assert(productLeft(List(2, 4, 6)) === 48)
     assert(productLeft(List(2, 4, 0)) === 0)
   }
 
+  // Exercise 3.11
   test("lengthLeft") {
     assert(lengthLeft(List(1, 2, 3)) === 3)
     assert(lengthLeft(Nil) === 0)
   }
 
+  // Exercise 3.12
   test("reverse") {
     assert(reverse(List(1, 2, 3)) === List(3, 2, 1))
   }
 
+  // Exercise 3.13
   test("foldLeftViaFoldRight") {
     assert(foldLeftViaFoldRight(List(1, 2, 3), 0)(_ + _) === 6)
   }
 
+  // Exercise 3.13
   test("foldRightViaFoldLeft") {
     assert(foldRightViaFoldLeft(List(1, 2, 3), 0)(_ + _) === 6)
   }
 
+  // Exercise 3.14
   test("appendFold") {
     assert(appendFold(List(1, 2, 3), List(4, 5)) === List(1, 2, 3, 4, 5))
   }
 
+  // Exercise 3.15
   test("concat") {
     assert(concat(List(List(1), List(2, 3, 4), Nil)) === List(1, 2, 3, 4))
   }
 
+  // Exercise 3.16
   test("increment") {
     assert(increment(List(1, 2, 4)) === List(2, 3, 5))
   }
 
+  // Exercise 3.17
   test("doubleToString") {
     assert(doubleToString(List(1, 2, 5)) === List("1.0", "2.0", "5.0"))
   }
 
+  // Exercise 3.18
   test("map") {
     assert(map(List(1, 2, 3))(_ * 2) === List(2, 4, 6))
   }
 
+  // Exercise 3.19
   test("filter") {
     assert(filter(List(1, 2, 3, 4, 5))(_ % 2 == 0) === List(2, 4))
   }
 
+  // Exercise 3.20
   test("flatMap") {
     assert(flatMap(List(1,2,3))(i => List(i,i)) === List(1,1,2,2,3,3))
   }
 
+  // Exercise 3.21
   test("filterViaFlatMap") {
     assert(filter(List(1, 2, 3, 4, 5))(_ % 2 == 0) === List(2, 4))
   }
 
+  // Exercise 3.22
   test("addList") {
     assert(addList(List(1, 2, 3), List(4, 5, 6)) === List(5, 7, 9))
   }
 
+  // Exercise 3.23
   test("zipWith") {
     assert(zipWith(List(1, 2, 3), List(4, 5, 6))(List(_, _)) === List(List(1, 4), List(2, 5), List(3, 6)))
     assert(zipWith(List("a", "b"), List("c", "d"))(_ + _) === List("ac", "bd"))
   }
 
+  // Exercise 3.24
   test("startsWith") {
     assert(startsWith(List(1, 2, 3), List(1, 2)) === true)
     assert(startsWith(List(1, 2, 3), List(2, 1)) === false)
@@ -122,6 +140,7 @@ class ListSuite extends FunSuite {
     assert(hasSubsequence(List(1, 2, 3, 4), List(4)) === true)
   }
 
+  // Exercise 3.24
   test("hasSubsequence") {
     assert(hasSubsequence(List(1, 2, 3, 4), List(1, 2)) === true)
     assert(hasSubsequence(List(1, 2, 3, 4), List(2, 3)) === true)
--- src/test/scala/ch3datastructures/TreeSuite.scala
+++ src/test/scala/ch3datastructures/TreeSuite.scala
@@ -6,16 +6,19 @@ import Tree._
 
 class TreeSuite extends FunSuite {
 
+  // Exercise 3.25
   test("size") {
     assert(size(Leaf('a')) === 1)
     assert(size(Branch(Leaf(2), Branch(Branch(Leaf(5), Leaf(6)), Leaf(7)))) === 7)
   }
 
+  // Exercise 3.26
   test("maximum") {
     assert(maximum(Leaf(5)) === 5)
     assert(maximum(Branch(Leaf(4), Branch(Leaf(12), Leaf(7)))) === 12)
   }
 
+  // Exercise 3.27
   test("depth") {
     assert(depth(Branch(Leaf(4), Branch(Leaf(12), Leaf(7)))) === 3)
   }
