785eefa5d8faa2f6f7e789b59502312ace56230f
Jérémie Astori <jeremie@astori.fr>
2014-12-28 17:30:09 -0500

    Exercise 4.1: map, getOrElse, flatMap, orElse and filter for Option

--- src/main/scala/ch4errorhandling/Option.scala
+++ src/main/scala/ch4errorhandling/Option.scala
@@ -1,5 +1,53 @@
 package ch4errorhandling
 
-sealed trait Option[+A]
+sealed trait Option[+A] {
+
+  // Exercise 4.1
+  // Implement all of the preceding functions on Option. As you implement each
+  // function, try to think about what it means and in what situations you’d use
+  // it. We’ll explore when to use each of these functions next. Here are a few
+  // hints for solving this exercise:
+  // - It’s fine to use pattern matching, though you should be able to implement
+  // all  the functions besides map and getOrElse without resorting to pattern
+  // matching.
+  // - For map and flatMap, the type signature should be enough to determine the
+  // implementation.
+  // - getOrElse returns the result inside the Some case of the Option, or if the
+  // Option is None, returns the given default value.
+  // - orElse returns the first Option if it’s defined; otherwise, it returns the
+  // second Option.
+
+  def map[B](f: A => B): Option[B] = this match {
+    case None => None
+    case Some(a) => Some(f(a))
+  }
+
+  def getOrElse[B >: A](default: => B): B = this match {
+    case None => default
+    case Some(a) => a
+  }
+
+  def flatMap[B](f: A => Option[B]): Option[B] =
+    map(f) getOrElse None
+    // this match {
+    //   case None => None
+    //   case Some(a) => f(a)
+    // }
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] =
+    map(Some(_)) getOrElse ob
+    // this match {
+    //   case None => ob
+    //   case _ => this
+    // }
+
+  def filter(f: A => Boolean): Option[A] =
+    flatMap((a: A) => if (f(a)) Some(a) else None)
+    // this match {
+    //   case Some(a) if (f(a)) => Some(a)
+    //   case _ => None
+    // }
+}
+
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
--- src/test/scala/ch4errorhandling/OptionSuite.scala
+++ src/test/scala/ch4errorhandling/OptionSuite.scala
@@ -2,8 +2,36 @@ package ch4errorhandling
 
 import org.scalatest.FunSuite
 
-import Option._
-
 class OptionSuite extends FunSuite {
 
+  // Exercise 4.1
+  test("map") {
+    assert(Some(3).map(_ * 2) === Some(6))
+    assert((None: Option[Int]).map(_ * 2) === None)
+  }
+
+  // Exercise 4.1
+  test("getOrElse") {
+    assert(Some(42).getOrElse(13) === 42)
+    assert(None.getOrElse(13) === 13)
+  }
+
+  // Exercise 4.1
+  test("flatMap") {
+    assert(Some(42).flatMap(Some(_)) === Some(42))
+    assert(None.flatMap(Some(_)) === None)
+  }
+
+  // Exercise 4.1
+  test("orElse") {
+    assert(Some(42).orElse(Some(13)) === Some(42))
+    assert(None.orElse(Some(13)) === Some(13))
+  }
+
+  // Exercise 4.1
+  test("filter") {
+    assert(Some(42).filter(_ > 0) === Some(42))
+    assert(Some(-42).filter(_ > 0) === None)
+    assert((None: Option[Int]).filter(_ > 0) === None)
+  }
 }
