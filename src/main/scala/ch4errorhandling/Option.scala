617fccafa6538e8242d1d6f81c628bc520ad5dff
Jérémie Astori <jeremie@astori.fr>
2014-12-28 16:29:14 -0500

    Boilerplate for Option (chapter 4)

---
+++ src/main/scala/ch4errorhandling/Option.scala
@@ -0,0 +1,5 @@
+package ch4errorhandling
+
+sealed trait Option[+A]
+case class Some[+A](get: A) extends Option[A]
+case object None extends Option[Nothing]
---
+++ src/test/scala/ch4errorhandling/OptionSuite.scala
@@ -0,0 +1,9 @@
+package ch4errorhandling
+
+import org.scalatest.FunSuite
+
+import Option._
+
+class OptionSuite extends FunSuite {
+
+}
