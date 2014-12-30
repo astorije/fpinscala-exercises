42b7950ee09641524b666d1d264d46fbc5ca8805
Jérémie Astori <jeremie@astori.fr>
2014-12-30 00:50:50 -0500

    Boilerplate for Either (chapter 4)

---
+++ src/main/scala/ch4errorhandling/Either.scala
@@ -0,0 +1,9 @@
+package ch4errorhandling
+
+sealed trait Either[+E, +A]
+case class Left[+E](value: E) extends Either[E, Nothing]
+case class Right[+A](value: A) extends Either[Nothing, A]
+
+object Either {
+
+}
---
+++ src/test/scala/ch4errorhandling/EitherSuite.scala
@@ -0,0 +1,9 @@
+package ch4errorhandling
+
+import org.scalatest.FunSuite
+
+import Either._
+
+class EitherSuite extends FunSuite {
+
+}
