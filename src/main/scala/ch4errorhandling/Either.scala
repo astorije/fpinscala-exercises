4cef0c26b135d3fb2758c7912e91380e7a4c1600
Jérémie Astori <jeremie@astori.fr>
2014-12-30 00:52:07 -0500

    Exercise 4.6: map, flatMap, orElse and map2 for Either

--- src/main/scala/ch4errorhandling/Either.scala
+++ src/main/scala/ch4errorhandling/Either.scala
@@ -1,6 +1,33 @@
 package ch4errorhandling
 
-sealed trait Either[+E, +A]
+sealed trait Either[+E, +A] {
+
+  // Exercise 4.6
+  // Implement versions of map, flatMap, orElse, and map2 on Either that operate
+  // on the Right value.
+  def map[B](f: A => B): Either[E, B] = this match {
+    case Left(e) => Left(e)
+    case Right(a) => Right(f(a))
+  }
+
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Left(e) => Left(e)
+    case Right(a) => f(a)
+  }
+
+  def orElse[EE >: E,B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Left(_) => b
+    case Right(a) => Right(a)
+  }
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    this.flatMap(a => b.map(bb => f(a, bb)))
+    // for {
+    //   a <- this
+    //   b1 <- b
+    // } yield f(a,b1)
+}
+
 case class Left[+E](value: E) extends Either[E, Nothing]
 case class Right[+A](value: A) extends Either[Nothing, A]
 
--- src/test/scala/ch4errorhandling/EitherSuite.scala
+++ src/test/scala/ch4errorhandling/EitherSuite.scala
@@ -6,4 +6,28 @@ import Either._
 
 class EitherSuite extends FunSuite {
 
+  // Exercise 4.6
+  test("map") {
+    assert(Right(3).map(_ * 2) === Right(6))
+    assert((Left("Error"): Either[String, Int]).map(_ * 2) === Left("Error"))
+  }
+
+  // Exercise 4.6
+  test("flatMap") {
+    assert(Right(42).flatMap(Left(_)) === Left(42))
+    assert(Left(42).flatMap(Right(_)) === Left(42))
+  }
+
+  // Exercise 4.6
+  test("orElse") {
+    assert(Right(42).orElse(Right(0)) === Right(42))
+    assert(Left("Error").orElse(Right(42)) === Right(42))
+  }
+
+  // Exercise 4.6
+  test("map2") {
+    assert(Right(2).map2(Right(4))(_ * _) === Right(8))
+    assert(Right(2).map2(Left("Error"): Either[String, Int])(_ * _) === Left("Error"))
+    assert((Left("Error"): Either[String, Int]).map2(Right(4))(_ * _) === Left("Error"))
+  }
 }
