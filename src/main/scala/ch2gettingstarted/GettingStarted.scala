cba54df984b633c0c2db1c984aa4962d26a3fb85
Jérémie Astori <jeremie@astori.fr>
2015-01-03 01:19:26 -0500

    Fix old comment

--- src/main/scala/ch2gettingstarted/GettingStarted.scala
+++ src/main/scala/ch2gettingstarted/GettingStarted.scala
@@ -5,7 +5,7 @@ object GettingStarted {
   // Write a recursive function to get the nth Fibonacci number. The first two
   // Fibonacci numbers are 0 and 1. The nth number is always the sum of the
   // previous two - the sequence begins 0, 1, 1, 2, 3, 5. Your definition should
-  // // use a tail-recursive function
+  // use a tail-recursive function.
   def fib(n: Int): Int = {
     @annotation.tailrec
     def loop(previous: Int, current: Int, n: Int): Int =
