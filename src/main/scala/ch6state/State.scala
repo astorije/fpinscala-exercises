58fbcc4467e04cc10621ae13cd24546e59a2a803
Jérémie Astori <jeremie@astori.fr>
2015-01-04 13:10:39 -0500

    Boilerplate for State

---
+++ src/main/scala/ch6state/State.scala
@@ -0,0 +1,16 @@
+package ch6state
+
+trait RNG {
+  def nextInt: (Int, RNG)
+}
+
+object RNG {
+  case class Simple(seed: Long) extends RNG {
+    def nextInt: (Int, RNG) = {
+      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+      val nextRNG = Simple(newSeed)
+      val n = (newSeed >>> 16).toInt
+      (n, nextRNG)
+    }
+  }
+}
---
+++ src/test/scala/ch6state/StateSuite.scala
@@ -0,0 +1,9 @@
+package ch6state
+
+import org.scalatest.FunSuite
+
+import RNG._
+
+class StateSuite extends FunSuite {
+
+}
