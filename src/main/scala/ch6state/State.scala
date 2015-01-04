b6b5210843fe63ebd9e5c4b8742d8ae71258033d
Jérémie Astori <jeremie@astori.fr>
2015-01-04 13:29:07 -0500

    Exercise 6.1: nonNegativeInt

--- src/main/scala/ch6state/State.scala
+++ src/main/scala/ch6state/State.scala
@@ -13,4 +13,13 @@ object RNG {
       (n, nextRNG)
     }
   }
+
+  // Exercise 6.1
+  // Write a function that uses RNG.nextInt to generate a random integer between
+  // 0 and Int.maxValue (inclusive). Make sure to handle the corner case when
+  // nextInt returns Int.MinValue, which doesn’t have a non-negative counterpart.
+  def nonNegativeInt(rng: RNG): (Int, RNG) = rng.nextInt match {
+    case (i, r) if i < 0 => (-i-1, r)
+    case t => t
+  }
 }
--- src/test/scala/ch6state/StateSuite.scala
+++ src/test/scala/ch6state/StateSuite.scala
@@ -5,5 +5,8 @@ import org.scalatest.FunSuite
 import RNG._
 
 class StateSuite extends FunSuite {
-
+  // Exercise 6.1
+  test("nonNegativeInt") {
+    assert(nonNegativeInt(RNG.Simple(42L)) === (16159453, Simple(1059025964525L)))
+  }
 }
