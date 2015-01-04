0f863e5231af82596d4d306f1f3b326d764de68e
Jérémie Astori <jeremie@astori.fr>
2015-01-04 00:09:59 -0500

    Exercise 5.16: scanRight

--- src/main/scala/ch5laziness/Stream.scala
+++ src/main/scala/ch5laziness/Stream.scala
@@ -144,6 +144,24 @@ sealed trait Stream[+A] {
       case s @ Cons(_, t) => Some((s, t()))
       case Empty => None
     } append Stream(empty)
+
+  // Exercise 5.16
+  // Hard: Generalize tails to the function scanRight, which is like a foldRight
+  // that returns a stream of the intermediate results. For example:
+  // scala> Stream(1,2,3).scanRight(0)(_ + _).toList
+  // res0: List[Int] = List(6,5,3,0)
+  // This example should be equivalent to the expression List(1+2+3+0, 2+3+0, 3+0, 0).
+  // Your function should reuse intermediate results so that traversing a Stream
+  // with n elements always takes time linear in n. Can it be implemented using
+  // unfold? How, or why not? Could it be implemented using another function
+  // we’ve written?
+  def scanRight[B](z: B)(f: (A, => B) => B): Stream[B] =
+    // tails.map(_.foldRight(z)(f))
+    foldRight((z, Stream(z)))((a, b) => {
+      lazy val lb = b
+      val x = f(a, lb._1)
+      (x, cons(x, lb._2))
+    })._2
 }
 
 case object Empty extends Stream[Nothing]
--- src/test/scala/ch5laziness/StreamSuite.scala
+++ src/test/scala/ch5laziness/StreamSuite.scala
@@ -153,4 +153,9 @@ class StreamSuite extends FunSuite {
     assert(Stream(1,2,3).tails.map(_.toList).toList === List(List(1,2,3), List(2,3), List(3), List()))
     assert(empty[Any].tails.map(_.toList).toList === List(Nil))
   }
+
+  // Exercise 5.16
+  test("scanRight") {
+    assert(Stream(1,2,3).scanRight(0)(_ + _).toList === List(6,5,3,0))
+  }
 }
