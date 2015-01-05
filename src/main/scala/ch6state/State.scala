2da4e684879ba20605b75ddb03a3a5a4e290802a
Jérémie Astori <jeremie@astori.fr>
2015-01-05 00:09:57 -0500

    Exercise 6.3: intDouble, doubleInt, double3

--- src/main/scala/ch6state/State.scala
+++ src/main/scala/ch6state/State.scala
@@ -30,4 +30,26 @@ object RNG {
   def double(rng: RNG): (Double, RNG) = nonNegativeInt(rng) match {
     case (i, r) => ((i / (Int.MaxValue.toDouble + 1)), r)
   }
+
+  // Exercise 6.3
+  // Write functions to generate an (Int, Double) pair, a (Double, Int) pair,
+  // and a (Double, Double, Double) 3-tuple. You should be able to reuse the
+  // functions you’ve already written.
+  def intDouble(rng: RNG): ((Int,Double), RNG) = {
+    val (i, r1) = nonNegativeInt(rng)
+    val (d, r2) = double(r1)
+    ((i, d), r2)
+  }
+
+  def doubleInt(rng: RNG): ((Double,Int), RNG) = {
+    val ((i, d), r) = intDouble(rng)
+    ((d, i), r)
+  }
+
+  def double3(rng: RNG): ((Double,Double,Double), RNG) = {
+    val (d1, r1) = double(rng)
+    val (d2, r2) = double(r1)
+    val (d3, r3) = double(r2)
+    ((d1, d2, d3), r3)
+  }
 }
--- src/test/scala/ch6state/StateSuite.scala
+++ src/test/scala/ch6state/StateSuite.scala
@@ -14,4 +14,19 @@ class StateSuite extends FunSuite {
   test("double") {
     assert(double(RNG.Simple(42L))._1 === 0.007524831686168909)
   }
+
+  // Exercise 6.3
+  test("intDouble") {
+    assert(intDouble(RNG.Simple(42L))._1 === (16159453, 0.5967354848980904))
+  }
+
+  // Exercise 6.3
+  test("doubleInt") {
+    assert(doubleInt(RNG.Simple(42L))._1 === (0.5967354848980904, 16159453))
+  }
+
+  // Exercise 6.3
+  test("double3") {
+    assert(double3(RNG.Simple(42L))._1 === (0.007524831686168909, 0.5967354848980904, 0.15846728393808007))
+  }
 }
