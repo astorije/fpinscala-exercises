b78622ef282ee45d613247b46946f8b1e8e0f674
Jérémie Astori <jeremie@astori.fr>
2015-01-17 11:56:30 -0500

    Exercise 7.1: signature of map2

--- src/main/scala/ch7parallelism/Par.scala
+++ src/main/scala/ch7parallelism/Par.scala
@@ -3,5 +3,11 @@ package ch7parallelism
 import java.util.concurrent._
 
 object Par {
+  type Par[A] = Any
 
+  // Exercise 7.1
+  // Par.map2 is a new higher-order function for combining the result of two
+  // parallel computations. What is its signature? Give the most general
+  // signature possible (don’t assume it works only for Int).
+  def map2[A, B, C](pa: Par[A], pb: Par[B])(f: (A, B) => C): Par[C] = ???
 }
